c4d33fd192a336f9cdd7df1ea59a662b09c85b95
Lili <lilliamwitham>
2023-05-23 11:34:07 -0600

    deploy

--- src/components/reactions-taylor.component.js
+++ src/components/reactions-taylor.component.js
@@ -1,9 +1,18 @@
 import React from "react";
 import '../styles/taylor.css'
+import KafkaService from "../services/kafka.service";
 
 function ReactionsTaylorComponent() {
     return (
         <div class="reactions">
+        <button onClick={(e) => {
+                    e.preventDefault();
+                    saveLike(e, 1)
+                      
+                    }
+                } >
+                 Love
+              </button>
         <div class="reaction reaction-like"></div>
         <div class="reaction reaction-love"></div>
         <div class="reaction reaction-haha"></div>
@@ -13,5 +22,19 @@ function ReactionsTaylorComponent() {
         </div>
     );
 }
+
+function saveLike(e, status) {
+  
+    let data = {
+      id: 0,
+      status: status
+    };
+ 
+    console.log(JSON.stringify(data));
+ 
+    KafkaService.reaction("i-love-adsoftsito");
+    e.preventDefault();
+}
+
 
 export default ReactionsTaylorComponent;
